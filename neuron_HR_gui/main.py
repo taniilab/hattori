1558efdf0b631e4522d4896c7f272d211b180643
kanopero <p-hat69@fuji.waseda.jp>
2017-12-22 09:16:32 +0900

    steps

--- neuron_HR_gui/main.py
+++ neuron_HR_gui/main.py
@@ -7,6 +7,7 @@ from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+from neuron import Neuron_HR as Neuron
 
 
 class CentralWidget(QWidget):
@@ -22,7 +23,7 @@ class CentralWidget(QWidget):
         layout_glaph = QVBoxLayout()
         layout_glaph.addWidget(self.glaph)
         layout_glaph.addWidget(navi)
-        
+
         # HR model palm range
         self.a_max = 3
         self.a_min = -1
@@ -40,10 +41,14 @@ class CentralWidget(QWidget):
         self.xr_min = -3
         self.i_max = 10
         self.i_min = -10
-        
+        self.gcmp_max = 20
+        self.gcmp_min = 0
+        self.delay_max = 150
+        self.delay_min = 0
+
         self.slider_min = 1
         self.slider_max = 100
-        
+
         # HR model palameter
         layout1_2 = QHBoxLayout()
         layout1 = QVBoxLayout()
@@ -157,6 +162,34 @@ class CentralWidget(QWidget):
         layout8.addWidget(self.textbox8)
         layout8.addLayout(layout8_2)
 
+        layout9_2 = QHBoxLayout()
+        layout9 = QVBoxLayout()
+        self.slider9 = QSlider(Qt.Horizontal)
+        self.slider9.setRange(self.slider_min, self.slider_max)
+        self.slider9.setValue(int(self.glaph.gcmp - self.gcmp_min) /
+                              (self.gcmp_max - self.gcmp_min)*self.slider_max)
+        self.label9 = QLabel('gcmp :')
+        layout9_2.addWidget(self.label9)
+        layout9_2.addWidget(self.slider9)
+        self.textbox9 = QLineEdit()
+        self.textbox9.setText(str(self.glaph.gcmp))
+        layout9.addWidget(self.textbox9)
+        layout9.addLayout(layout9_2)
+
+        layout10_2 = QHBoxLayout()
+        layout10 = QVBoxLayout()
+        self.slider10 = QSlider(Qt.Horizontal)
+        self.slider10.setRange(self.slider_min, self.slider_max)
+        self.slider10.setValue(int(self.glaph.delay - self.delay_min) /
+                              (self.delay_max - self.delay_min)*self.slider_max)
+        self.label10 = QLabel('delay :')
+        layout10_2.addWidget(self.label10)
+        layout10_2.addWidget(self.slider10)
+        self.textbox10 = QLineEdit()
+        self.textbox10.setText(str(self.glaph.delay))
+        layout10.addWidget(self.textbox10)
+        layout10.addLayout(layout10_2)
+
         layout_palm = QVBoxLayout()
         layout_palm.addLayout(layout1)
         layout_palm.addLayout(layout2)
@@ -166,6 +199,8 @@ class CentralWidget(QWidget):
         layout_palm.addLayout(layout6)
         layout_palm.addLayout(layout7)
         layout_palm.addLayout(layout8)
+        layout_palm.addLayout(layout9)
+        layout_palm.addLayout(layout10)
 
         groupBox1 = QGroupBox("palm")
         groupBox2 = QGroupBox("glaph")
@@ -192,6 +227,8 @@ class CentralWidget(QWidget):
         self.textbox6.textChanged.connect(self.text6_changed)
         self.textbox7.textChanged.connect(self.text7_changed)
         self.textbox8.textChanged.connect(self.text8_changed)
+        self.textbox9.textChanged.connect(self.text9_changed)
+        self.textbox10.textChanged.connect(self.text10_changed)
         self.slider1.valueChanged.connect(self.slider1_changed)
         self.slider2.valueChanged.connect(self.slider2_changed)
         self.slider3.valueChanged.connect(self.slider3_changed)
@@ -200,6 +237,9 @@ class CentralWidget(QWidget):
         self.slider6.valueChanged.connect(self.slider6_changed)
         self.slider7.valueChanged.connect(self.slider7_changed)
         self.slider8.valueChanged.connect(self.slider8_changed)
+        self.slider9.valueChanged.connect(self.slider9_changed)
+        self.slider10.valueChanged.connect(self.slider10_changed)
+
 
     # slot
     def text1_changed(self):
@@ -250,6 +290,18 @@ class CentralWidget(QWidget):
         else:
             self.glaph.replot_i(float(self.textbox8.text()))
 
+    def text9_changed(self):
+        if self.textbox9.text() is "":
+            self.glaph.replot_gcmp(float(self.glaph.tmp_gcmp))
+        else:
+            self.glaph.replot_gcmp(float(self.textbox9.text()))
+
+    def text10_changed(self):
+        if self.textbox10.text() is "":
+            self.glaph.replot_delay(float(self.glaph.tmp_delay))
+        else:
+            self.glaph.replot_delay(float(self.textbox10.text()))
+
     def slider1_changed(self):
         self.s1palm = round((self.slider1.value()/self.slider_max) *
                              (self.a_max-self.a_min)+self.a_min, 3)
@@ -290,16 +342,26 @@ class CentralWidget(QWidget):
                           (self.i_max-self.i_min)+self.i_min)
         self.textbox8.setText(self.s8palm)
 
+    def slider9_changed(self):
+        self.s9palm = str((self.slider9.value()/self.slider_max) *
+                          (self.gcmp_max-self.gcmp_min)+self.gcmp_min)
+        self.textbox9.setText(self.s9palm)
+
+    def slider10_changed(self):
+        self.s10palm = str((self.slider10.value()/self.slider_max) *
+                           (self.delay_max-self.delay_min)+self.delay_min)
+        self.textbox10.setText(self.s10palm)
+
 
 class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.left = 10
-        self.top = 10
+        self.left = 100
+        self.top = 100
         self.title = 'Hindmarsh-Rose model - simulater'
-        self.width = 1024
-        self.height = 768
+        self.width = 2048
+        self.height = 1900
         self.initUI()
 
     def initUI(self):
@@ -313,81 +375,145 @@ class PlotCanvas(FigureCanvas):
 
     def __init__(self, parent=None, width=10, height=8, dpi=100):
         fig = Figure(figsize=(width, height), dpi=dpi)
-        self.axes = fig.add_subplot(111)
 
         FigureCanvas.__init__(self, fig)
         self.setParent(parent)
 
         FigureCanvas.setSizePolicy(self,
-                QSizePolicy.Expanding,
-                QSizePolicy.Expanding)
+                                   QSizePolicy.Expanding,
+                                   QSizePolicy.Expanding)
         FigureCanvas.updateGeometry(self)
         self.plot_init()
 
     def plot_init(self):
+        self.nr = Neuron()
+
         self.a = 1
         self.b = 3
         self.c = 1
         self.d = 5
         self.r = 0.003
         self.s = 4
-        self.i = 2.3
+        self.i = 0
         self.xr = -1.56
-        self.ax = self.figure.add_subplot(111)
-        self.ax.set_title('Hindmarsh-Rose model Example')
-        
-        self.dt = 0.05
-        self.t = np.arange(0, 2000, self.dt)
-        self.dx = 0
-        self.dy = 0
-        self.dz = 0
-        self.x = [-1.5] * np.size(self.t)
-        self.y = [-10.2] * np.size(self.t)
-        self.z = [-1.6] * np.size(self.t)
-        
-        self.plot(self.a, self.b, self.c, self.d, self.r, self.s, self.xr, self.i)
+        self.gcmp = 0
+        self.delay = 0
+
+        self.ax0 = self.figure.add_subplot(221)
+        self.ax1 = self.figure.add_subplot(222)
+        self.ax2 = self.figure.add_subplot(223)
+        self.ax3 = self.figure.add_subplot(224)
+        self.ax0.set_title('N0')
+        self.ax1.set_title('N1')
+        self.ax2.set_title('N2')
+        self.ax3.set_title('N3')
+        plt.title('Hindmarsh-Rose model')
+
+        self.tmp_a = self.a
+        self.tmp_b = self.b
+        self.tmp_c = self.c
+        self.tmp_d = self.d
+        self.tmp_r = self.r
+        self.tmp_s = self.s
+        self.tmp_xr = self.xr
+        self.tmp_i = self.i
+        self.tmp_gcmp = self.gcmp
+        self.tmp_delay = self.delay
+
+        self.plot(self.a, self.b, self.c, self.d, self.r, self.s, self.xr,
+                  self.i, self.gcmp, self.delay)
 
     def replot_a(self, a):
-        self.line.remove()
+        self.line0.remove()
+        self.line1.remove()
+        self.line2.remove()
+        self.line3.remove()
         self.tmp_a = a
-        self.plot(self.tmp_a, self.b, self.c, self.d, self.r, self.s, self.xr, self.i)
+        self.plot(self.tmp_a, self.b, self.c, self.d, self.r, self.s, self.xr,
+                  self.i, self.gcmp, self.delay)
 
     def replot_b(self, b):
-        self.line.remove()     
+        self.line0.remove()
+        self.line1.remove()
+        self.line2.remove()
+        self.line3.remove()
         self.tmp_b = b
-        self.plot(self.a, self.tmp_b, self.c, self.d, self.r, self.s, self.xr, self.i)
+        self.plot(self.a, self.tmp_b, self.c, self.d, self.r, self.s, self.xr,
+                  self.i, self.gcmp, self.delay)
 
     def replot_c(self, c):
-        self.line.remove()     
-        self.tmp_c = c 
-        self.plot(self.a, self.b, self.tmp_c, self.d, self.r, self.s, self.xr, self.i)
+        self.line0.remove()
+        self.line1.remove()
+        self.line2.remove()
+        self.line3.remove()
+        self.tmp_c = c
+        self.plot(self.a, self.b, self.tmp_c, self.d, self.r, self.s, self.xr,
+                  self.i, self.gcmp, self.delay)
 
     def replot_d(self, d):
-        self.line.remove()     
+        self.line0.remove()
+        self.line1.remove()
+        self.line2.remove()
+        self.line3.remove()
         self.tmp_d = d
-        self.plot(self.a, self.b, self.c, self.tmp_d, self.r, self.s, self.xr, self.i)
+        self.plot(self.a, self.b, self.c, self.tmp_d, self.r, self.s, self.xr,
+                  self.i, self.gcmp, self.delay)
 
     def replot_r(self, r):
-        self.line.remove()     
+        self.line0.remove()
+        self.line1.remove()
+        self.line2.remove()
+        self.line3.remove()
         self.tmp_r = r
-        self.plot(self.a, self.b, self.c, self.d, self.tmp_r, self.s, self.xr, self.i)
+        self.plot(self.a, self.b, self.c, self.d, self.tmp_r, self.s, self.xr,
+                  self.i, self.gcmp, self.delay)
 
     def replot_s(self, s):
-        self.line.remove()     
+        self.line0.remove()
+        self.line1.remove()
+        self.line2.remove()
+        self.line3.remove()
         self.tmp_s = s
-        self.plot(self.a, self.b, self.c, self.d, self.r, self.tmp_s, self.xr, self.i)
+        self.plot(self.a, self.b, self.c, self.d, self.r, self.tmp_s, self.xr,
+                  self.i, self.gcmp, self.delay)
 
     def replot_xr(self, xr):
-        self.line.remove()     
+        self.line0.remove()
+        self.line1.remove()
+        self.line2.remove()
+        self.line3.remove()
         self.tmp_xr = xr
-        self.plot(self.a, self.b, self.c, self.d, self.r, self.s, self.tmp_xr, self.i)
+        self.plot(self.a, self.b, self.c, self.d, self.r, self.s, self.tmp_xr,
+                  self.i, self.gcmp, self.delay)
 
     def replot_i(self, i):
-        self.line.remove()     
-        self.tmp_i = i 
-        self.plot(self.a, self.b, self.c, self.d, self.r, self.s, self.xr, self.tmp_i)
+        self.line0.remove()
+        self.line1.remove()
+        self.line2.remove()
+        self.line3.remove()
+        self.tmp_i = i
+        self.plot(self.a, self.b, self.c, self.d, self.r, self.s, self.xr,
+                  self.tmp_i, self.gcmp, self.delay)
 
-    def plot(self, a, b, c, d, r, s, xr, i):
+    def replot_gcmp(self, gcmp):
+        self.line0.remove()
+        self.line1.remove()
+        self.line2.remove()
+        self.line3.remove()
+        self.tmp_gcmp = gcmp
+        self.plot(self.a, self.b, self.c, self.d, self.r, self.s, self.xr,
+                  self.i, self.tmp_gcmp, self.delay)
+
+    def replot_delay(self, delay):
+        self.line0.remove()
+        self.line1.remove()
+        self.line2.remove()
+        self.line3.remove()
+        self.tmp_delay = delay
+        self.plot(self.a, self.b, self.c, self.d, self.r, self.s, self.xr,
+                  self.i, self.gcmp, self.tmp_delay)
+
+    def plot(self, a, b, c, d, r, s, xr, i, gcmp, delay):
         self.a = a
         self.b = b
         self.c = c
@@ -396,17 +522,44 @@ class PlotCanvas(FigureCanvas):
         self.s = s
         self.xr = xr
         self.i = i
+        self.gcmp = gcmp
+        self.delay = delay
 
-        for i in range(0, np.size(self.t)-1):
-            self.dx = self.y[i] - self.a * self.x[i]**3 + self.b * self.x[i]**2 -self.z[i] + self.i
-            self.dy = self.c - self.d * self.x[i]**2 - self.y[i]
-            self.dz = self.r*(self.s*(self.x[i] - self.xr) - self.z[i])
+        self.number = 4
 
-            self.x[i+1] = self.x[i] + self.dt * self.dx
-            self.y[i+1] = self.y[i] + self.dt * self.dy
-            self.z[i+1] = self.z[i] + self.dt * self.dz
 
-        self.line, = self.ax.plot(self.t, self.x, "b")
+        self.palm = {"numneu": self.number,
+                     "a": self.a,
+                     "b": self.b,
+                     "c": self.c,
+                     "d": self.d,
+                     "r": self.r,
+                     "s": self.s,
+                     "xr": self.xr,
+                     "gcmp": self.gcmp,
+                     "delay":self.delay,
+                     "Iext_amp": self.i,
+                     "Iext_width": 10,
+                     "Iext_num": 1000,
+                     "Syncp": 5,
+                     "noise": 1}
+
+        self.nr.__init__(**self.palm)
+
+        
+        for i in range(0, self.nr.allsteps-1):
+            if (self.nr.curstep * self.nr.dt) > 200:
+                for j in range(self.number-1):
+                    self.nr.cnct[j, j+1] = 1.0
+                    self.nr.cnct[j+1, j] = 1.0
+                pass
+            self.nr.propagation()
+
+        self.line0, = self.ax0.plot(self.nr.tmhist, self.nr.x[0], "b")
+        self.line1, = self.ax1.plot(self.nr.tmhist, self.nr.x[1], "b")
+        self.line2, = self.ax2.plot(self.nr.tmhist, self.nr.x[2], "b")
+        self.line3, = self.ax3.plot(self.nr.tmhist, self.nr.x[3], "b")
+
         self.draw()
 
 if __name__ == '__main__':
