ed9ae38b709e87e96f78446ffe31e4f87b30c95c
kanopero <p-hat69@fuji.waseda.jp>
2019-12-25 15:30:26 +0900

    steps

---
+++ Tool/main.py
@@ -0,0 +1,17 @@
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from ddplot import Ui_MainWindow
+
+
+class Main(QMainWindow):
+    def __init__(self, parent=None):
+        super(Main, self).__init__(parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = Main()
+    window.show()
+    sys.exit(app.exec_())
