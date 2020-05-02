4fe55bd896d5d5e80e2b324e59a451334c7438e5
kanopero <p-hat69@fuji.waseda.jp>
2020-05-02 18:41:22 +0900

    steps

--- FY2020/fixpoint/試験問題/main.py
+++ FY2020/fixpoint/試験問題/main.py
@@ -8,16 +8,15 @@ import datetime
 year_start = 2005
 month_start = 4
 day_start = 10
-
 year_end = 2017
 month_end = 4
 day_end = 30
 start_date = datetime.datetime(year_start, month_start, day_start)
 end_date = datetime.datetime(year_end, month_end, day_end)
-##################
-
+### Parameters ###
 read_path = "./var/log/httpd/*_log"
 lump_size = 5
+##################
 timezone_label = np.arange(0, 24, 1)
 timezone_list =[]
 for i in range(24):
@@ -28,6 +27,7 @@ def main():
     lump_init_line = 0
     files = glob.glob(read_path)
 
+    # process files every lump size
     for j in range(len(files)):
         df = pd.DataFrame(np.zeros((lump_size, 1)))
         while len(df) == lump_size:
@@ -41,11 +41,16 @@ def main():
                 break
         lump_init_line = 0
 
+    # save
     remotehost_list_T = remotehost_list.transpose()
-    remotehost_list_T = remotehost_list_T.rename(columns={'0':'traffic'})
-    print(timezone_list)
-    print(remotehost_list)
-    print(remotehost_list_T)
+    remotehost_list_T = remotehost_list_T.rename(columns={'0': 'traffic'})
+    remotehost_list_T.to_csv('./remotehost.csv', header=None)
+    df = pd.DataFrame()
+    df['timezone'] = timezone_label
+    df['traffic'] = timezone_list
+    df.to_csv('./timezone.csv')
+
+    # visualize
     plt.figure(figsize=(10, 5))
     plt.bar(timezone_label, timezone_list)
     plt.xlabel('remotehost')
@@ -66,19 +71,18 @@ def process(data):
         if date < start_date or date > end_date:
             drop_list.append(i)
     data = data.drop(drop_list)
-    print(data)
 
     # count the number of accesses by time zone
     for i in range(len(data)):
         hour = data.iat[i, 3].split(':')[1]
         timezone_list[int(hour)] += 1
-    print(timezone_list)
 
     # count the number of accesses by remote host
     for i in range(len(data)):
         if not data.iat[i, 0] in remotehost_list.columns:
             remotehost_list[data.iat[i, 0]] = [0]
         remotehost_list[data.iat[i, 0]] += 1
+
     print(remotehost_list)
     print("****************************")
 
