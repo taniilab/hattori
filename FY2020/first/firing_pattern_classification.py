5601a1ab8d1aa3aeba51458213cdc4aa21d07d03
kanopero <p-hat69@fuji.waseda.jp>
2020-10-10 09:54:14 +0900

    steps

--- FY2020/first/firing_pattern_classification.py
+++ FY2020/first/firing_pattern_classification.py
@@ -15,11 +15,12 @@ import matplotlib as mpl
 from multiprocessing import Pool
 from statistics import mean, median,variance,stdev
 import re
+import itertools
 
 
 # for overflow error
 mpl.rcParams['agg.path.chunksize'] = 100000
-
+process = 20
 
 class Picture():
     def __init__(self,
@@ -29,6 +30,7 @@ class Picture():
         self.nowdir = path
         self.csvs = path + '/' + '*.csv'
         self.files = {}
+        self.counter = 0
         self.files = glob.glob(self.csvs)
         print(self.files)
         self.d = datetime.datetime.today()
@@ -40,20 +42,25 @@ class Picture():
 
     def run2(self, value):
         self.value = value
+        cv_list = []
+        ampa_list = []
+        nmda_list = []
+
         for file_ in self.csv_tmp_list[self.value]:
+            print(self.csv_tmp_list[self.value])
             df_title = pd.read_csv(file_, nrows=1)
             df = pd.read_csv(file_, index_col=0, skiprows=1)
+            print(file_)
             spike = df['fire_0']
             spike = spike.values
             spike_time_list = []
             isi = []
-            cv = []
 
             filename = os.path.basename(file_).replace('.csv', '')
             filename = re.findall("P_AMPA[0-9]\.[0-9]_P_NMDA[0-9]\.[0-9]", filename)[0]
             print(filename)
-            p_ampa = float(filename[6:9])
-            p_nmda = float(filename[16:19])
+            ampa_list.append(int(float(filename[6:9])*10))
+            nmda_list.append(int(float(filename[16:19])*10))
 
             for i in range(len(spike)):
                 if spike[i] != 0:
@@ -61,15 +68,17 @@ class Picture():
 
             for i in range(len(spike_time_list)-1):
                 isi.append(spike_time_list[i+1]-spike_time_list[i])
-            print("isi: {}".format(isi))
-            print(mean(isi))
-            cv = variance(isi)/mean(isi)
-            print(cv)
+            try:
+                #cv = variance(isi[5:]) / mean(isi[5:])
+                cv = variance(isi) / mean(isi)
+            except:
+                cv = 0
+            cv_list.append(cv)
 
             print(str(self.counter) + '個目のファイルを処理します')
             self.counter += 1
 
-        return cv, int(p_ampa*10), int(p_nmda*10)
+        return cv_list, ampa_list, nmda_list
 
     def run(self):
         num_ampa = 16
@@ -81,12 +90,10 @@ class Picture():
         self.csv_tmp_list = list(zip(*[iter(self.files)] * int(self.unit_files)))
         pool = Pool(self.process)
         res = pool.map(self.run2, range(self.process))
+        print(res)
 
-        for i in range(len(res)):
-            print(res[i][1])
-            print(res[i][2])
-            print(res[i][0])
-            cv_hist[res[i][1]][res[i][2]] += res[i][0]
+        for i, j in itertools.product(range(process), range(int(len(self.files)/process))):
+            cv_hist[res[i][1][j]][res[i][2][j]] += res[i][0][j]
 
         cv_hist /= 5
         for i in range(num_ampa):
@@ -94,11 +101,13 @@ class Picture():
         for j in range(num_nmda):
             list_columns.append("NMDA: "+str(round(j*0.1, 1)))
         df = pd.DataFrame(cv_hist, columns=list_columns, index=list_rows)
+        print("kashikoma")
         df.to_csv(self.nowdir+"/cv/heatmap_cv.csv")
 
 def main():
     save_path = "C:/sim/raw_data/Mg16/raw_data/2020_10_9_10_50_53"
-    process = 20
+    #save_path = "C:/sim/raw_data/test"
+
     numneu = 1
     pic = Picture(save_path, process, numneu)
     pic.run()
