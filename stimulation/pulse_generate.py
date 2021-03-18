1733ad7f228e9161287fb908904446e9a9c1bb5e
kanopero <p-hat69@fuji.waseda.jp>
2021-03-18 14:35:36 +0900

    steps

--- stimulation/pulse_generate.py
+++ stimulation/pulse_generate.py
@@ -1,98 +1,100 @@
 import time
 path = "C:/Users/Neuron/Documents/GitHub/hattori/stimulation/"
+savename = '24_monopolar_100mL_200mhz.fy'
 x = ""
 counter = 0
 
+#バイポーラ
 """
-#theta burst stimulation 1set
-for i in range(20):
-    x += "1\n"
-    counter += 1
-for i in range(389):
-    x += "0\n"
-    counter += 1
-for i in range(20):
-    x += "1\n"
-    counter += 1
-for i in range(389):
-    x += "0\n"
-    counter += 1
-for i in range(20):
-    x += "1\n"
-for i in range(389):
-    x += "0\n"
-    counter += 1
-for i in range(20):
-    x += "1\n"
-    counter += 1
-for i in range(389):
-    x += "0\n"
-    counter += 1
-for i in range(20):
-    x += "1\n"
-    counter += 1
-for i in range(389):
-    x += "0\n"
-    counter += 1
-for i in range(6167):
-    x += "0\n"
-    counter += 1
-"""
-
-#テタヌス
-for j in range(100):
+for i in range(164):
     x += "-1\n"
     counter += 1
+for i in range(164):
     x += "1\n"
     counter += 1
-    for i in range(80):
-        x += "0\n"
-        counter += 1
+for i in range(7864):
+    x += "0\n"
+    counter += 1
+"""
 
-#theta burst stimulation 10set→分解能不足
+#モノポーラ
+for i in range(164):
+    x += "-1\n"
+    counter += 1
+for i in range(1):
+    x += "1\n"
+    counter += 1
+for i in range(8027):
+    x += "0\n"
+    counter += 1
+
+#テタヌス
 """
-for j in range(10):
-    for i in range(2):
+for j in range(100):
+    for i in range(10):
+        x += "-1\n"
+        counter += 1
+    for i in range(10):
         x += "1\n"
         counter += 1
-    for i in range(39):
+    for i in range(62):
         x += "0\n"
         counter += 1
-    for i in range(2):
-        x += "1\n"
-        counter += 1
-    for i in range(39):
-        x += "0\n"
-        counter += 1
-    for i in range(2):
-        x += "1\n"
-        counter += 1
-    for i in range(39):
-        x += "0\n"
-        counter += 1
-    for i in range(2):
-        x += "1\n"
-        counter += 1
-    for i in range(39):
-        x += "0\n"
-        counter += 1
-    for i in range(2):
-        x += "1\n"
-        counter += 1
-    for i in range(39):
-        x += "0\n"
-        counter += 1
-    for i in range(614):
-        x += "0\n"
-        counter += 1
-x += "0\n"
-counter += 1
-x += "0\n"
-counter += 1
 """
+#シータバースト
+"""
+for j in range(5):
+    for i in range(10):
+        x += "-1\n"
+        counter += 1
+    for i in range(10):
+        x += "1\n"
+        counter += 1
+    for i in range(62):
+        x += "0\n"
+        counter += 1
+    for i in range(10):
+        x += "-1\n"
+        counter += 1
+    for i in range(10):
+        x += "1\n"
+        counter += 1
+    for i in range(62):
+        x += "0\n"
+        counter += 1
+    for i in range(10):
+        x += "-1\n"
+        counter += 1
+    for i in range(10):
+        x += "1\n"
+        counter += 1
+    for i in range(62):
+        x += "0\n"
+        counter += 1
+    for i in range(10):
+        x += "-1\n"
+        counter += 1
+    for i in range(10):
+        x += "1\n"
+        counter += 1
+    for i in range(62):
+        x += "0\n"
+        counter += 1
+    for i in range(10):
+        x += "-1\n"
+        counter += 1
+    for i in range(10):
+        x += "1\n"
+        counter += 1
+    for i in range(62):
+        x += "0\n"
+        counter += 1
+    for i in range(1228):
+        x += "0\n"
+        counter += 1
+"""
+
 print(counter)
-#print(x)
-#f = open(path+'TBS_0.5m_9.5m_5cycle_200mhz_.txt', 'w')
-f = open(path+'tetanus_100hz_1s.fy', 'w')
+f = open(path+savename, 'w')
 f.write(x)
 f.close()
